9d9cb6b9115e15173fbb4915fe2bbd97a167d452
Jake Gilger <10113305+JakeGilger@users.noreply.github.com>
2019-05-01 15:54:13 -0700

    add another test for Problem 1

--- Problem1/solution.py
+++ Problem1/solution.py
@@ -10,6 +10,7 @@ def test():
     assert numk([10, 15, 3, 7], 17)==True, "Given example"
 
     assert numk([10, -1, 12, 5], 9)==True, "Negative numbers"
+    assert numk([6, 2, 7, 4], 3)==False, "Not found case"
     print("Tests Passed!")
 
 test()
